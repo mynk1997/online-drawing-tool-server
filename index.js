9b9d0edd8affee6449e4d4d7645a1bdb3815e914
Mayank <mynk1997@gmail.com>
2023-11-16 14:25:55 +0530

    Server setup with socket to client

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors= require("cors");
+const cors = require('cors');
 
 const app = express();
-app.use(cors({origin: 'https://localhost:3000'}))
+const isDev = app.settings.env === 'development'
+const URL =  'http://localhost:3000'
+app.use(cors({origin: URL}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors:'https://localhost:3000' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
-  console.log("Server connected on port: 3002")
+  console.log("server connected")
+
+  socket.on('beginPath', (arg) => {
+    socket.broadcast.emit('beginPath', arg)
+  })
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg)
+  })
+
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg)
+  })
 });
 
 httpServer.listen(3002);
